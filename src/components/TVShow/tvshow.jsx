ad0f40400adfb13dd730e0745a8acbdc44dd621c
FRIN Yvonnick <frin.yvonnick@gmail.com>
2016-09-26 11:08:32 +0200

    Enhance episode style

--- src/components/TVShow/tvshow.jsx
+++ src/components/TVShow/tvshow.jsx
@@ -7,13 +7,21 @@ const TVShow = ({ id, name, image, summary }) => {
   return (
     <div className={styles.tvshow}>
       <h1>{name}</h1>
-      <h2>En résumé</h2>
-      <div className={styles.abstract}>
-        <img role="presentation" src={image} />
-        <div className={styles.summary} dangerouslySetInnerHTML={{ __html: summary }} />
+			<div className="card resume">
+				<h2>En résumé</h2>
+				<div className="card-content">
+					<div className={styles.abstract}>
+						<img role="presentation" src={image} />
+						<div className={styles.summary} dangerouslySetInnerHTML={{ __html: summary }} />
+					</div>
+    		</div>
+   		</div>
+      <div className="card episodes">
+				<h2>Episodes</h2>
+				<div className="card-content">
+    			<Episodes id={id} />
+    		</div>
       </div>
-      <h2>Episodes</h2>
-      <Episodes id={id} />
     </div>
   )
 }
