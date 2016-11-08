4da10ab46fe0f29583490233f10ede7d5153c730
Fabien JUIF <fabien.juif@zenika.com>
2016-11-08 09:15:52 +0100

    Step7: init

--- src/components/TVShow/Episodes/episodes.selectors.js
+++ src/components/TVShow/Episodes/episodes.selectors.js
@@ -1,23 +1,12 @@
 import sortBy from 'lodash/sortBy'
-import { createSelector } from 'reselect'
 import { getId } from '../../../redux/router'
-import { helpers, getTVShows } from '../../../redux/tvshows'
+import { helpers } from '../../../redux/tvshows'
 import { defaultArray } from '../../../redux/defaults'
 
-export const getEpisodes = createSelector(
-  [getId, getTVShows],
-  (id, tvshows) => {
-    const tvshow = helpers.getById({ tvshows }, Number(id))
-    const episodes = tvshow.episodes || defaultArray
-
-    return sortBy(episodes, ['season', 'number']).reverse()
-  }
-)
-
-/* export const getEpisodes = (state) => {
+export const getEpisodes = (state) => {
   const id = Number(getId(state))
   const tvshow = helpers.getById(state, Number(id))
   const episodes = tvshow.episodes || defaultArray
 
   return sortBy(episodes, ['season', 'number']).reverse()
-} */
+}
